c236d5933b5bd5265bfa82c5140e1968da8c4493
LGP <651011260@qq.com>
2019-07-06 16:06:13 +0800

    init cloud config.

---
+++ core/cloud-config-client/src/main/java/com/awu/raft/core/cloud/config/client/CloudConfigClientApplication.java
@@ -0,0 +1,15 @@
+package com.awu.raft.core.cloud.config.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author LGP
+ */
+@SpringBootApplication
+public class CloudConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudConfigClientApplication.class, args);
+    }
+}
